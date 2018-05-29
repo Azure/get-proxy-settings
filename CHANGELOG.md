dad34b9da85932363c2df2573b9514f93fc740b6
Timothee Guerin <timothee.guerin@outlook.com>
2018-05-29 10:01:46 -0700

    Fix: ProxyEnable being ignored (#16)

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Version 0.1.11
+
+Fixes:
+ * Seems to ignore the ProxyEnable registry field [#16](https://github.com/Azure/get-proxy-settings/pull/16)
--- src/proxy.spec.ts
+++ src/proxy.spec.ts
@@ -1,6 +1,65 @@
 import { expect } from "chai";
+import * as proxyquire from "proxyquire";
 import { parseWindowsProxySetting } from "./proxy";
+import { ProxySetting } from "./proxy-settings";
+
 describe("proxy", () => {
+    describe("getProxyWindows", () => {
+        let winregData = {};
+        let proxySpy;
+        beforeEach(() => {
+            proxySpy = proxyquire("./proxy", {
+                "./winreg": {
+                    openKey: () => {
+                        return Promise.resolve(winregData);
+                    },
+                },
+            });
+        });
+
+        it("returns null when no proxy settings defined at all", async () => {
+            const proxy = await proxySpy.getProxyWindows();
+            expect(proxy).to.eq(null);
+        });
+
+        it("returns null when proxy enabled is 0x0", async () => {
+            winregData = {
+                ProxyEnable: { value: "0x0" },
+                ProxyServer: { value: "localhost:8888" },
+            };
+            const proxy = await proxySpy.getProxyWindows();
+            expect(proxy).to.eq(null);
+        });
+
+        it("returns null when proxy enabled is 0x00000000", async () => {
+            winregData = {
+                ProxyEnable: { value: "0x00000000" },
+                ProxyServer: { value: "localhost:8888" },
+            };
+            const proxy = await proxySpy.getProxyWindows();
+            expect(proxy).to.eq(null);
+        });
+
+        it("returns proxy settings when proxy enabled is 0x1", async () => {
+            winregData = {
+                ProxyEnable: { value: "0x1" },
+                ProxyServer: { value: "localhost:8888" },
+            };
+            const proxy = await proxySpy.getProxyWindows();
+            expect(proxy).not.to.eq(null);
+
+            expect(proxy.http).not.to.eq(null);
+            expect(proxy.http).instanceOf(ProxySetting);
+            expect(proxy.http.host).to.equal("localhost");
+            expect(proxy.http.port).to.equal("8888");
+
+            expect(proxy.https).not.to.eq(null);
+            expect(proxy.https).instanceOf(ProxySetting);
+            expect(proxy.https.host).to.equal("localhost");
+            expect(proxy.https.port).to.equal("8888");
+        });
+    });
+
     describe("parseWindowsProxySetting", () => {
         it("should return null if null or undefined", () => {
             expect(parseWindowsProxySetting(null)).to.be.null;
